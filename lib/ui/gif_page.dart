4f5ff0bd86b7adf52e43785bceee621e1967ba32
Renan Toniolo Rocha <renantoniolo@gmail.com>
2019-07-30 00:11:03 -0300

    commit final
    proejto concluido

---
+++ lib/ui/gif_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+
+class GifPage extends StatelessWidget {
+  final Map _gifData;
+
+  GifPage(this._gifData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          this._gifData["title"],
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.yellow,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: (){
+                Share.share(this._gifData["images"]["fixed_height"]["url"]);
+            },
+          )
+        ],
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.network(this._gifData["images"]["fixed_height"]["url"]),
+      ),
+    );
+  }
+}
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -17,7 +19,7 @@ class _HomePageState extends State<HomePage> {
 
     if (_search == null)
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/trending?api_key=yT1lbaIoJL99aveNtJOKB2xO5DVcdVpe&limit=20&rating=G");
+          "https://api.giphy.com/v1/gifs/trending?api_key=yT1lbaIoJL99aveNtJOKB2xO5DVcdVpe&limit=19&rating=G");
     else
       response = await http.get(
           "https://api.giphy.com/v1/gifs/search?api_key=yT1lbaIoJL99aveNtJOKB2xO5DVcdVpe&q=$_search&limit=20&offset=$_offset&rating=G&lang=pt");
@@ -50,19 +52,17 @@ class _HomePageState extends State<HomePage> {
             padding: EdgeInsets.all(10.0),
             child: TextField(
               decoration: InputDecoration(
-                prefixText: "-",
                   labelText: "Pesquise Aqui",
                   labelStyle: TextStyle(color: Colors.white),
                   border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
-              
               onSubmitted: (text) {
                 setState(() {
                   _search = text;
+                  _offset = 0;
                   _getGifs();
                 });
-                
               },
             ),
           ),
@@ -96,19 +96,65 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  int _getCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
   Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
     return GridView.builder(
         padding: EdgeInsets.all(10.0),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
-        itemCount: snapshot.data["data"].length,
+        itemCount: _getCount(snapshot.data["data"]),
         itemBuilder: (context, index) {
-          return GestureDetector(
+          if (_search == null || index < snapshot.data["data"].length) {
+            return GestureDetector(
               child: Image.network(
-            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-            height: 250.0,
-            fit: BoxFit.cover,
-          ));
+                snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                height: 250.0,
+                fit: BoxFit.cover,
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            GifPage(snapshot.data["data"][index])));
+              },
+              onLongPress: () {
+                Share.share(
+                  snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                );
+              },
+            );
+          } else {
+            return Container(
+              child: GestureDetector(
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 70.0,
+                    ),
+                    Text(
+                      "Carregar mais...",
+                      style: TextStyle(fontSize: 22.0, color: Colors.white),
+                    ),
+                  ],
+                ),
+                onTap: () {
+                  setState(() {
+                    _offset += 19;
+                  });
+                },
+              ),
+            );
+          }
         });
   }
 }
